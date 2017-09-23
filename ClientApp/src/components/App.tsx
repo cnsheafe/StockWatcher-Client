a6aef2aa4e1b193466ad249a9a5834dbcdae1f27
cnsheafe <cnsheafe@gmail.com>
2017-09-23 15:53:08 -0600

    removed unneccesary imports

--- ClientApp/src/components/App.tsx
+++ ClientApp/src/components/App.tsx
@@ -1,16 +1,9 @@
 import * as React from "react";
 import Header from "./Header";
 import { Body } from "./Body";
-import {connect} from "react-redux";
-import store, {IState} from "../store/store";
-import { toggleModalDisplay } from "../store/actions";
-
-interface AppProps {
-  showModal: boolean
-}
 
 
-export default class App extends React.Component<AppProps, {}> {
+export default class App extends React.Component<{}, {}> {
   render() {
     return (
       <main>
